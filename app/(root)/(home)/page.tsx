56065197b0fb3988fe04491c865205f69697ce55
itzgaurav2005 <133147299+itzgaurav2005@users.noreply.github.com>
2025-02-15 21:08:33 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,7 @@
 import MeetingTypeList from '@/components/MeetingTypeList';
 import React from 'react';
 
-const Home = ({ params }: { params: { id: string } }) => {
-  console.log(params.id); // Ensure it logs the correct ID
+const Home = () => {
   const now = new Date();
   const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
   const date = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(now);
