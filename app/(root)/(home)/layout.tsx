b55bf5b14ceee5f902d274fda90dafec81e47a9e
itzgaurav2005 <133147299+itzgaurav2005@users.noreply.github.com>
2025-02-15 20:54:24 +0530

    Update layout.tsx

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,7 +1,7 @@
-import Navbar from '@/components/navbar'
-import Sidebar from '@/components/sidebar'
+import Navbar from '@/components/navbar';
+import Sidebar from '@/components/sidebar';
 import { Metadata } from 'next';
-import React from 'react'
+import React from 'react';
 
 export const metadata: Metadata = {
   title: "YOOM",
@@ -11,22 +11,20 @@ export const metadata: Metadata = {
   }
 };
 
-export const HomeLayout = ({children} : {children : React.ReactNode}) => {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className='relative'>
-        < Navbar />
+      <Navbar />
 
-        <div className='flex'>
-            < Sidebar />
+      <div className='flex'>
+        <Sidebar />
 
-            <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
-                <div className='w-full'>
-                     {children}
-                </div>
-            </section>
-        </div>
+        <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
+          <div className='w-full'>
+            {children}
+          </div>
+        </section>
+      </div>
     </main>
-  )
+  );
 }
-
-export default HomeLayout
